5172053d8c40aaf59b073c671a621310c792425f
江山 <363085078@qq.com>
2020-10-18 22:58:12 +0800

    新增一个聚光灯的功能，通过点击按钮控制开启

---
+++ ExcelAddIn_VSTO/Ribbon_VSTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace ExcelAddIn_VSTO
+{
+    public partial class Ribbon_VSTO
+    {
+        private void Ribbon_VSTO_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            //点击按钮改变if_jgd_open的状态
+            if (Properties.Settings.Default.if_jgd_open == false) 
+            {
+                Properties.Settings.Default.if_jgd_open = true;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.if_jgd_open = false;
+                Properties.Settings.Default.Save();
+                Globals.ThisAddIn.Application.Cells.Interior.ColorIndex = 0;
+            }
+        }
+    }
+}
--- ExcelAddIn_VSTO/ThisAddIn.cs
+++ ExcelAddIn_VSTO/ThisAddIn.cs
@@ -6,21 +6,24 @@ using System.Xml.Linq;
 using Excel = Microsoft.Office.Interop.Excel;
 using Office = Microsoft.Office.Core;
 using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Interop.Excel;
 
 namespace ExcelAddIn_VSTO
 {
+    
     public partial class ThisAddIn
     {
         Excel.Application ExcelApp;     //声明一个Excel的应用程序对象
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
             ExcelApp = Globals.ThisAddIn.Application;      //获取到加载项所在Excel应用程序
-            ExcelApp.ActiveCell.Value = "11";        //测试給活动单元格输入新的值    
+            //ExcelApp.ActiveCell.Value = "11";        //测试給活动单元格输入新的值    
+
         }
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
         }
-
+        
         #region VSTO 生成的代码
 
         /// <summary>
@@ -31,8 +34,25 @@ namespace ExcelAddIn_VSTO
         {
             this.Startup += new System.EventHandler(ThisAddIn_Startup);
             this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+            Globals.ThisAddIn.Application.SheetSelectionChange += Application_SheetSelectionChange;
         }
-        
+
+        private void Application_SheetSelectionChange(object Sh, Range Target)
+        {
+            //throw new NotImplementedException();
+            if (Properties.Settings.Default.if_jgd_open == true) 
+            {
+                ExcelApp.Cells.Interior.ColorIndex = 0;
+                Range target = Globals.ThisAddIn.Application.Selection;
+                if (target.Count > 1)
+                {
+                    target = target.Cells[1];
+                }
+                target.EntireColumn.Interior.ColorIndex = 6;
+                target.EntireRow.Interior.ColorIndex = 6;
+            }
+        }
+
         #endregion
     }
 }
